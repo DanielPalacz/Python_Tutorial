0ce1817b2c9b5076b79f3e533cc7338ef5d8c78a
DanielPalacz <daniel.palacz@gmail.com>
2020-12-27 15:55:11 +0100

    Christmas update

--- Other_Python_Related_notes/BuiltinFunctions_review/map_zip_filter_reduce.py
+++ Other_Python_Related_notes/BuiltinFunctions_review/map_zip_filter_reduce.py
@@ -3,3 +3,17 @@ list1 = [1, 2, 3]
 
 list2 = list(map(lambda x: x*2, list1))
 print(list2)
+
+a_list = [1, 2, 3, 4]
+b_list = [5, 6, 7]
+print(list(zip(a_list, b_list)))
+# [(1, 5), (2, 6), (3, 7)]
+
+c_list = [8, 9]
+print(list(zip(a_list, b_list, c_list)))
+# [(1, 5, 8), (2, 6, 9)]
+
+filtered_list = list(filter(lambda x: x > 2, a_list))
+print(filtered_list)
+# [3, 4]
+
