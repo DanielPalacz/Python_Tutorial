802ad7e7c6db419fd6b929e32de297da827ee05a
DanielPalacz <daniel.palacz@gmail.com>
2021-02-26 19:09:26 +0100

    request - part 1

---
+++ requests/1_get_ResponseObject_statusCode.py
@@ -0,0 +1,14 @@
+import requests
+import json # noqa
+
+# A Response is a powerful object for inspecting the results of the request.
+get_http_response = requests.get('https://api.github.com')
+# captured the return value of get(), which is an instance of 'Response', and stored it in a variable called response.
+# You can now use response to see a lot of information about the results of your GET request.
+
+#
+# 1. Status code:
+
+
+print(get_http_response.status_code)
+get_http_response.raise_for_status()
